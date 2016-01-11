bcda37d70024bb694738326ad28dd20f875fc85b
Abie MBP <joenabie@me.com>
2016-01-11 10:05:33 +0800

    clickEvent

--- app/src/main/java/com/example/abiegamao/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/abiegamao/myapplication/HomeScreen.java
@@ -1,7 +1,9 @@
 package com.example.abiegamao.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends ActionBarActivity {
 
@@ -9,5 +11,19 @@ public class HomeScreen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void clickEvent(View v){
+
+        switch(v.getId()){
+
+            case R.id.btnchangepin:
+                Intent goToPinChanger = new Intent(HomeScreen.this, PinChanger.class);
+                startActivity(goToPinChanger);
+
+
+        }
+
+
     }
 }
