9b4aa28145aabd3d93aad25ed39dfac71c3b8868
Abie MBP <joenabie@me.com>
2016-01-11 09:36:35 +0800

    Initial commit ;) Yay

---
+++ app/src/main/java/com/example/abiegamao/myapplication/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.abiegamao.myapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    //NumberPicker n1,n2,n3,n4;
+    NumberPicker[] np = new NumberPicker[4];
+    String str;
+    //STORED PREFERENCES
+    public static final String pinPref = "pinPref";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        np[0] = (NumberPicker) findViewById(R.id.numberpicker1);
+        np[1] = (NumberPicker) findViewById(R.id.numberpicker2);
+        np[2] = (NumberPicker) findViewById(R.id.numberpicker3);
+        np[3] = (NumberPicker) findViewById(R.id.numberpicker4);
+
+        for(int i=0;i<4;i++){
+            //str = "R.id.numberpicker" + (i+1);
+            np[i].setMinValue(0);
+            np[i].setMaxValue(9);
+
+
+        }
+
+
+    }
+
+
+    public void clickEvent(View v){
+        int[] p = new int[4];
+
+        SharedPreferences defpref = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
+
+        //DEFAULT CODE : 0000
+        for(int i=0;i<4;i++){
+            str = "pin" + i+1;
+            p[i] = defpref.getInt(str,0);
+        }
+
+        if (p[0]== np[0].getValue() && p[1]== np[1].getValue() && p[2]== np[2].getValue() && p[3]== np[3].getValue()) {
+            Toast.makeText(this, "Right.", Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/abiegamao/myapplication/PinChanger.java
@@ -0,0 +1,13 @@
+package com.example.abiegamao.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class PinChanger extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_changer);
+    }
+}
