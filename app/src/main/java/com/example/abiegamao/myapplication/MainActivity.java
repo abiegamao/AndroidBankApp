2b7f0a6227428d011ea1e30abfefee18b028c91f
Abie <Joenabie@me.com>
2016-01-11 14:40:39 +0800

    //Change pin -Cyril

--- app/src/main/java/com/example/abiegamao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/abiegamao/myapplication/MainActivity.java
@@ -18,15 +18,15 @@ public class MainActivity extends ActionBarActivity {
 
     //NumberPicker n1,n2,n3,n4;
     NumberPicker[] np = new NumberPicker[4];
-    String str;
     //STORED PREFERENCES
     public static final String pinPref = "pinPref";
-
+    public SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPref = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
 
         np[0] = (NumberPicker) findViewById(R.id.numberpicker1);
         np[1] = (NumberPicker) findViewById(R.id.numberpicker2);
@@ -47,15 +47,16 @@ public class MainActivity extends ActionBarActivity {
 
     public void clickEvent(View v){
         int[] p = new int[4];
-        SharedPreferences sharedPref;
-        sharedPref = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
-
 
         //DEFAULT CODE : 0000
-        for(int i=0;i<4;i++){
+        /*for(int i=0;i<4;i++){
             str = "pin" + i+1;
             p[i] = sharedPref.getInt(str,0);
-        }
+        }*/
+        p[0] = sharedPref.getInt("pin1",0);
+        p[1] = sharedPref.getInt("pin2",0);
+        p[2] = sharedPref.getInt("pin3",0);
+        p[3] = sharedPref.getInt("pin4",0);
 
         if (p[0]== np[0].getValue() && p[1]== np[1].getValue() && p[2]== np[2].getValue() && p[3]== np[3].getValue()) {
             Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/abiegamao/myapplication/PinChanger.java
+++ app/src/main/java/com/example/abiegamao/myapplication/PinChanger.java
@@ -13,13 +13,13 @@ public class PinChanger extends ActionBarActivity {
 
     public static final String pinPref = "pinPref";
     String str;
-   // SharedPreferences sharedPref;
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_changer);
-        //sharedPref = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
+        sharedPref = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
 
         //Numpicker Up [NP SET 1]
         np[0] = (NumberPicker) findViewById(R.id.numberpicker1);
@@ -35,7 +35,13 @@ public class PinChanger extends ActionBarActivity {
 
         for(int i=0;i<8;i++){
             np[i].setMinValue(0);
+            np[i].setValue(0);
             np[i].setMaxValue(9);
+        }
+        //disable NP SET 2 if pass not correct
+        for(int i=4; i < 8; i++){
+            np[i].setEnabled(false);
+
         }
     }
 
@@ -44,19 +50,19 @@ public class PinChanger extends ActionBarActivity {
 
         int[] p = new int[4];
 
-        SharedPreferences s = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
+       sharedPref = getSharedPreferences(pinPref, Context.MODE_PRIVATE);
 
         //default 0 savedPref
-        for(int i=0;i<4;i++){
+        /*for(int i=0;i<4;i++){
             str = "pin" + i+1;
-            p[i] = s.getInt(str,0);
-        }
+            p[i] = sharedPref.getInt(str,0);
+        }*/
+        p[0] = sharedPref.getInt("pin1",0);
+        p[1] = sharedPref.getInt("pin2",0);
+        p[2] = sharedPref.getInt("pin3",0);
+        p[3] = sharedPref.getInt("pin4",0);
+        Toast.makeText(this ,String.valueOf(sharedPref.getInt("pin1",0)) + String.valueOf(sharedPref.getInt("pin2",0)) + String.valueOf(sharedPref.getInt("pin3",0)) + String.valueOf(sharedPref.getInt("pin4",0)), Toast.LENGTH_SHORT).show();
 
-        //disable NP SET 2 if pass not correct
-        for(int i=4;i<8;i++){
-            np[i].setEnabled(false);
-
-        }
         //check from savedPref
         if (p[0]== np[0].getValue() && p[1]== np[1].getValue() && p[2]== np[2].getValue() && p[3]== np[3].getValue()) {
             Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
@@ -65,7 +71,7 @@ public class PinChanger extends ActionBarActivity {
             //NP Set 2 Enable change pin
             for(int i=4;i<8;i++){
                 np[i].setEnabled(true);
-                np[i].setValue(7);
+                np[i].setValue(0);
             }
 
 
@@ -80,7 +86,6 @@ public class PinChanger extends ActionBarActivity {
     public void successChange(View v){
 
         if (np[4].isEnabled()) {
-            SharedPreferences sharedPref = getSharedPreferences(pinPref,Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPref.edit();  //modifying savedPref
             int x = 1;
 
@@ -88,10 +93,15 @@ public class PinChanger extends ActionBarActivity {
 
 
             editor.putInt("pin1", np[4].getValue());
-
+            editor.apply();
+            editor.putInt("pin2", np[5].getValue());
+            editor.apply();
+            editor.putInt("pin3", np[6].getValue());
+            editor.apply();
+            editor.putInt("pin4", np[7].getValue());
             editor.apply();
 
-
+            Toast.makeText(this ,String.valueOf(np[4].getValue()) + String.valueOf(np[5].getValue())+ String.valueOf(np[6].getValue())+ String.valueOf(np[7].getValue()), Toast.LENGTH_SHORT).show();
             Toast.makeText(this, "PIN SUCCESSFULLY CHANGED!", Toast.LENGTH_SHORT).show();
             finish();
         }
