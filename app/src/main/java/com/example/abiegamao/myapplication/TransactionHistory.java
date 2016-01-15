b34288e3ce4ee4688b60aecc30467a8ede4fa07e
jmgamao <joenabie@me.com>
2016-01-15 14:07:22 +0800

    th updated - abie

--- app/src/main/java/com/example/abiegamao/myapplication/TransactionHistory.java
+++ app/src/main/java/com/example/abiegamao/myapplication/TransactionHistory.java
@@ -1,13 +1,15 @@
 package com.example.abiegamao.myapplication;
 
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TransactionHistory extends AppCompatActivity {
     EditText curr;
-    EditText editAmount;
+    TextView strCurrBal;
     DatabaseHelper helper;
 
     @Override
@@ -16,8 +18,22 @@ public class TransactionHistory extends AppCompatActivity {
         setContentView(R.layout.activity_transaction_history);
 
         helper = new DatabaseHelper(this);
+        strCurrBal=(TextView) findViewById(R.id.textViewCurrBalanceTH);
         curr = (EditText) findViewById(R.id.editTextCurrBalTH);
         curr.setEnabled(false);
+
+        //STR
+        Typeface gotham = Typeface.createFromAsset(getAssets(), "fonts/Gotham-Book-Regular.ttf");
+        Typeface funsized = Typeface.createFromAsset(getAssets(), "fonts/FunSized.ttf");
+        Typeface comix = Typeface.createFromAsset(getAssets(), "fonts/Comix-Loud.ttf");
+        Typeface fishFingers = Typeface.createFromAsset(getAssets(), "fonts/Fishfingers.ttf");
+
+        curr.setTypeface(fishFingers);
+        strCurrBal.setTypeface(comix);
+        curr.setTextSize(25);
+        strCurrBal.setTextSize(15);
+        strCurrBal.setText("CURRENT BALANCE: ");
+
 
         //GET CURRENT BALANCE DEP-WITHDRAW
         getCurrBal();
